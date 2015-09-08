25ae4b17b07a6a992c53cc90bc886e4ba567f915
gumbomasta <gregheff@gmail.com>
2015-09-08 12:12:18 -0400

    try again...

---
+++ Assets/MouseLock.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseLock : MonoBehaviour {
+
+	// Use this for initialization
+
+	public CursorLockMode wantedMode;
+
+
+
+	void Start () {
+
+		Cursor.lockState = wantedMode = CursorLockMode.Locked;
+	
+	}
+
+	/*void SetCursorState ()
+	{
+		Cursor.lockState = wantedMode;
+		// Hide cursor when locking
+		Cursor.visible = (CursorLockMode.Locked != wantedMode);
+	}
+*/
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown("escape"))
+			print("escape key was hit");
+			Cursor.lockState = wantedMode = CursorLockMode.Locked;
+
+		//Cursor.lockState = wantedMode = CursorLockMode.Locked;
+
+
+
+	}
+}
