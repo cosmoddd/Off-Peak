c26c24a1e6b13d749cdf7d4eeff0e8b3f732a3ef
gumbomasta <gregheff@gmail.com>
2015-09-08 17:27:52 -0400

    home stretch

---
+++ Assets/Buildr/Scripts/Utils/ExportMaterial.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace BuildRUtil
+{
+    public struct ExportMaterial
+    {
+        public string name;
+        public Material material;
+        public string filepath;
+        public bool generated;
+    }
+}
