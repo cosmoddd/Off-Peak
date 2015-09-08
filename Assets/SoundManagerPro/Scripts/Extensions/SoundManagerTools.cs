8e1b2b7ddd291472a934cf265d601fcd7a535400
gumbomasta <gregheff@gmail.com>
2015-09-08 09:19:45 -0400

    first off-peak push

---
+++ Assets/SoundManagerPro/Scripts/Extensions/SoundManagerTools.cs
@@ -0,0 +1,98 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using System;
+using System.Linq;
+
+public static class SoundManagerTools {
+	static readonly System.Random random = new System.Random();
+	public static void Shuffle<T> ( ref List<T> theList )
+	{
+		int n = theList.Count;
+		while (n > 1)
+		{
+			n--;
+			int k = random.Next(n + 1);
+			T val = theList[k];
+			theList[k] = theList[n];
+			theList[n] = val;
+		}
+	}
+	
+	public static void ShuffleTwo<T, K> ( ref List<T> theList, ref List<K> otherList)
+	{
+		int n = theList.Count;
+		while (n > 1)
+		{
+			n--;
+			int k = random.Next(n + 1);
+			T val = theList[k];
+			theList[k] = theList[n];
+			theList[n] = val;
+			
+			if(otherList.Count != theList.Count)
+			{
+				Debug.LogError("Can't shuffle both lists because this " + typeof(T).ToString() + " list doesn't have the same amount of items.");
+				continue;
+			}
+			K otherVal = otherList[k];
+			otherList[k] = otherList[n];
+			otherList[n] = otherVal;
+		}
+	}
+	
+	public static void make2D ( ref AudioSource theAudioSource )
+	{
+		theAudioSource.spatialBlend = 0f;
+	}
+	
+	public static void make3D ( ref AudioSource theAudioSource )
+	{
+		theAudioSource.spatialBlend = 1f;
+	}
+	
+	public static float VaryWithRestrictions ( this float theFloat, float variance, float minimum=0f, float maximum=1f)
+	{
+		float max = theFloat * (1f+variance);
+		float min = theFloat * (1f-variance);
+		
+		if(max > maximum)
+			max = maximum;
+		if(min < minimum)
+			min = minimum;
+		
+		return UnityEngine.Random.Range(min, max);
+	}
+	
+	public static float Vary ( this float theFloat, float variance)
+	{
+		float max = theFloat * (1f+variance);
+		float min = theFloat * (1f-variance);
+		
+		return UnityEngine.Random.Range(min, max);
+	}
+	
+#if !(UNITY_WP8 || UNITY_METRO)
+	/// <summary>
+	/// Returns all instance fields on an object, including inherited fields
+	/// http://stackoverflow.com/a/1155549/154165
+	/// </summary>
+	public static FieldInfo[] GetAllFieldInfos(this Type type)
+	{
+		if(type == null)
+			return new FieldInfo[0];
+
+		BindingFlags flags = 
+			BindingFlags.Public | 
+			BindingFlags.NonPublic | 
+			BindingFlags.Instance | 
+			BindingFlags.DeclaredOnly;
+
+		return type.GetFields(flags)
+			.Concat(GetAllFieldInfos(type.BaseType))
+			.Where(f => !f.IsDefined(typeof(HideInInspector), true))
+			.ToArray();
+	}
+#endif
+}
---
+++ Assets/SoundManagerPro/Scripts/Managers/SoundManager_Variables_SFX.cs
@@ -0,0 +1,155 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using antilunchbox;
+
+public partial class SoundManager : antilunchbox.Singleton<SoundManager> {
+
+	// Path to folder where SFX are held in resources
+	public string resourcesPath = "Sounds/SFX";
+
+	// List of local AudioClip SFXs added in inspector or through StoreSFX()
+	public List<AudioClip> storedSFXs = new List<AudioClip>();
+
+	// List of other gameobjects with SFX attached
+	public List<GameObject> unOwnedSFXObjects = new List<GameObject>();
+
+	// Dictionary of instance ID to cappedID to keep track of capped SFX
+	public Dictionary<int, string> cappedSFXObjects = new Dictionary<int, string>();
+	
+	// Dictionary of delayed audiosources
+	public Dictionary<AudioSource, float> delayedAudioSources = new Dictionary<AudioSource, float>();
+	
+	// Dictionary of sfx with runonendfunctions
+	public Dictionary<AudioSource, SongCallBack> runOnEndFunctions = new Dictionary<AudioSource, SongCallBack>();
+	
+	private AudioSource duckSource;
+	private SongCallBack duckFunction;
+	private bool isDucking = false;
+	private int duckNumber = 0;
+	private float preDuckVolume = 1f;
+	private float preDuckVolumeMusic = 1f;
+	private float preDuckVolumeSFX = 1f;
+	private float preDuckPitch = 1f;
+	private float preDuckPitchMusic = 1f;
+	private float preDuckPitchSFX = 1f;
+	
+	public static float duckStartSpeed = .1f;
+	public static float duckEndSpeed = .5f;
+	
+	// List of SFX groups
+	public List<SFXGroup> sfxGroups = new List<SFXGroup>();
+	
+	// Map of clip names to group names (dictionaries and hashtables are not supported for serialization)
+	public List<string> clipToGroupKeys = new List<string>();
+	public List<string> clipToGroupValues = new List<string>();
+	
+	private Dictionary<string, SFXGroup> groups = new Dictionary<string, SFXGroup>();
+	private Dictionary<string, string> clipsInGroups = new Dictionary<string, string>();
+	private Dictionary<string, AudioClip> allClips = new Dictionary<string, AudioClip>();
+	private Dictionary<string, int> prepools = new Dictionary<string, int>();
+	private Dictionary<string, float> baseVolumes = new Dictionary<string, float>();
+	private Dictionary<string, float> volumeVariations = new Dictionary<string, float>();
+	private Dictionary<string, float> pitchVariations = new Dictionary<string, float>();
+	
+	public bool offTheSFX = false;
+	public int capAmount = 3;
+
+	public float volumeSFX {
+		get{
+			return _volumeSFX;
+		} set {
+			foreach(KeyValuePair<AudioClip, SFXPoolInfo> pair in Instance.ownedPools)
+			{
+				foreach(GameObject ownedSFXObject in pair.Value.ownedAudioClipPool)
+				{
+					if(ownedSFXObject != null)
+						if(ownedSFXObject.GetComponent<AudioSource>() != null && (!isDucking || ownedSFXObject.GetComponent<AudioSource>() != duckSource))
+							ownedSFXObject.GetComponent<AudioSource>().volume = value;
+				}
+			}
+			foreach(GameObject unOwnedSFXObject in Instance.unOwnedSFXObjects)
+			{
+				if(unOwnedSFXObject != null)
+					if(unOwnedSFXObject.GetComponent<AudioSource>() != null && (!isDucking || unOwnedSFXObject.GetComponent<AudioSource>() != duckSource))
+						unOwnedSFXObject.GetComponent<AudioSource>().volume = value;
+			}
+			_volumeSFX = value;
+		}
+	}
+	private float _volumeSFX = 1f;
+
+	public float pitchSFX {
+		get{
+			return _pitchSFX;
+		} set {
+			foreach(KeyValuePair<AudioClip, SFXPoolInfo> pair in Instance.ownedPools)
+			{
+				foreach(GameObject ownedSFXObject in pair.Value.ownedAudioClipPool)
+				{
+					if(ownedSFXObject != null)
+						if(ownedSFXObject.GetComponent<AudioSource>() != null && (!isDucking || ownedSFXObject.GetComponent<AudioSource>() != duckSource))
+							ownedSFXObject.GetComponent<AudioSource>().pitch = value;
+				}
+			}
+			foreach(GameObject unOwnedSFXObject in Instance.unOwnedSFXObjects)
+			{
+				if(unOwnedSFXObject != null)
+					if(unOwnedSFXObject.GetComponent<AudioSource>() != null && (!isDucking || unOwnedSFXObject.GetComponent<AudioSource>() != duckSource))
+						unOwnedSFXObject.GetComponent<AudioSource>().pitch = value;
+			}
+			_pitchSFX = value;
+		}
+	}
+	private float _pitchSFX = 1f;
+
+	public float maxSFXVolume {
+		get{
+			return _maxSFXVolume;
+		} set {
+			_maxSFXVolume = value;
+		}
+	}
+	private float _maxSFXVolume = 1f;
+
+	public bool mutedSFX {
+		get {
+			return _mutedSFX;
+		} set {
+			foreach(KeyValuePair<AudioClip, SFXPoolInfo> pair in Instance.ownedPools)
+			{
+				foreach(GameObject ownedSFXObject in pair.Value.ownedAudioClipPool)
+				{
+					if(ownedSFXObject != null)
+						if(ownedSFXObject.GetComponent<AudioSource>() != null)
+							if(value)
+								ownedSFXObject.GetComponent<AudioSource>().mute = value;
+							else
+								if(!Instance.offTheSFX)
+									ownedSFXObject.GetComponent<AudioSource>().mute = value;
+				}
+			}
+			foreach(GameObject unOwnedSFXObject in Instance.unOwnedSFXObjects)
+			{
+				if(unOwnedSFXObject != null)
+					if(unOwnedSFXObject.GetComponent<AudioSource>() != null)
+						if(value)
+							unOwnedSFXObject.GetComponent<AudioSource>().mute = value;
+						else
+							if(!Instance.offTheSFX)
+								unOwnedSFXObject.GetComponent<AudioSource>().mute = value;
+			}
+			_mutedSFX = value;
+		}
+	}
+	private bool _mutedSFX = false;
+	
+	private Dictionary<AudioClip, SFXPoolInfo> ownedPools = new Dictionary<AudioClip, SFXPoolInfo>();
+	public List<int> sfxPrePoolAmounts = new List<int>();
+	public List<float> sfxBaseVolumes = new List<float>();
+	public List<float> sfxVolumeVariations = new List<float>();
+	public List<float> sfxPitchVariations = new List<float>();
+	public float SFXObjectLifetime = 10f;
+	
+	public List<string> currentPockets = new List<string>() { "Default" };
+}
